da9b64bbf0b392c64fcf43ecab721565ceb18e54
SakethVangara <sakethsai.vangara@gmail.com>
2023-11-22 11:14:09 -0500

    Add files via upload

---
+++ galaga_game.py
@@ -0,0 +1,24 @@
+# Names: Saketh Vangara, Hudson Alther
+# Computing IDs: ssc2ry, dvu7jh
+
+import uvage
+
+screen_width = 800
+screen_height = 600
+camera = uvage.Camera(screen_width, screen_height)
+
+game_on = True
+class Game:
+    global camera
+    def __init__(self):
+        pass
+
+    def run(self):
+        camera.display()
+
+def tick():
+    game = Game()
+    if game_on:
+        game.run()
+
+uvage.timer_loop(30, tick)
---
+++ project_checkpoint_1.py
@@ -0,0 +1,58 @@
+# Names: Saketh Vangara, Hudson Alther
+# Computing IDs (respectively): ssc2ry, dvu7jh
+
+                # DESCRIPTION OF OUR GAME #
+
+# The game that we plan on creating, or rather recreating,
+# is Galaga, a fixed shooter game where the user controls a spaceship
+# and attempts to destroy the Galaga (enemy) ships. The main objective
+# of the game is to defeat the Galaga forces, If the enemy hits the user,
+# then the user loses a life. The game ends when the user loses all of their
+# lives, so the goal is to both survive and defeat the enemy spaceships.
+
+                # BASIC FEATURES #
+# User Input: We will incorporate user input by allowing the
+# user to control the keyboard, specifically the left and right arrows,
+# to move their spaceship left and right across the bottom of the screen,
+# respectively.
+
+# Game Over: The user will have three lives. Every time the user
+# gets hit, they will lose a life, and if they lose all three lives,
+# the game is over. After the user loses all three lives, a screen
+# showing the text "Game Over" will appear in the middle of the screen
+
+# Graphics/Images: The main graphic that we will probably use
+# is a starry space background, since our game takes place in space.
+# We plan on implementing a different space-themed background
+# for every wave (level) of the game.
+
+
+            # ADDITIONAL FEATURES #
+# Restart from Game Over: When the game is over
+# (user runs out of lives), there will be an option for
+# the user to restart the game by pressing the "Restart" button
+# on the game over screen. In this way, they would not have to re-run
+# the program, as the game would start up again from the beginning.
+
+# Sprite Animation: To represent spaceship that the user controls
+# and the enemy spaceships, we plan on using sprite sheets from the
+# original game. In this way, we can program the gameboxes so that
+# they are animated.
+
+# Enemies: We will have multiple enemy types that the user will
+# have to try and defeat. For now, we will implement two
+# enemy types: Galaga (basic enemy) and Boss Galaga (main enemies).
+# If time permitting, we may try to add our own twist by creating
+# our own enemy types, but for now, we will stick to the aforementioned
+# enemy types.
+
+# Multiple Levels: Given the nature of the original Galaga game,
+# there are multiple levels, specifically waves, that the user
+# has to get through. Similarly, in our version of the game,
+# we will incorporate multiple levels for the user to pass,
+# each level containing a greater difficulty than the last and
+# possibly different enemy types (or more enemies to defeat in general).
+# For now, our plan is to make 3 to 5 waves, but if there is more time,
+# we will consider adding more waves so that the game can be played longer.
+
+
