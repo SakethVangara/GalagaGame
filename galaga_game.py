23c2d02ba174aac10925be7e3826d74ff7545fc5
HudsonAlther <151951740+HudsonAlther@users.noreply.github.com>
2023-11-24 14:03:50 -0500

    Update galaga_game.py

--- galaga_game.py
+++ galaga_game.py
@@ -3,9 +3,19 @@
 
 import uvage
 
+walls = [
+    uvage.from_color(300, 0, "black", 1000, 20),
+    uvage.from_color(400, 600, "black", 1000, 20),
+
+]
+
 screen_width = 800
 screen_height = 600
 camera = uvage.Camera(screen_width, screen_height)
+#game_images = uvage.load_sprite_sheet("Arcade - Galaga - General Sprites.png",11,25) backup code
+#player = uvage.from_image(400,300,game_images[6]) backup code
+player = uvage.from_image(400,550,"ship.png")
+player.scale_by(0.1)
 
 game_on = True
 class Game:
@@ -18,8 +28,19 @@ class Game:
         camera.display()
 
 def tick():
+    player.xspeed = 400
+    player.x = player.xspeed
     game = Game()
     if game_on:
         game.run()
+    if uvage.is_pressing("a"):
+        player.x += -7
+    if uvage.is_pressing("d"):
+        player.x += 7
 
+
+
+
+    camera.draw(player)
+    camera.display()
 uvage.timer_loop(30, tick)
